098fd819781eff4bfbb5f01908c3b7a66a8ff619
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-10-29 14:44:25 +0000

    7 eventListeners completed

--- js/index.js
+++ js/index.js
@@ -24,42 +24,38 @@ document.addEventListener('keydown', () => {
 //wheel
 
 window.addEventListener('wheel', (event) => {
-    console.log(event.clientY);
+    // console.log(event.clientY);
 })
-
-//drag / drop
-
-document.addEventListener('dragstart', function (event) {
-    event.dataTransfer.setData('Text', event.target.id)
-})
-
-document.addEventListener('dragover', (event) => {
-    event.preventDefault();
-})
-
-document.addEventListener("drop", function(event) {
-    event.preventDefault();
-    if ( event.target.className == "dropzone" ) {
-      document.getElementById("demo").style.color = "";
-      event.target.style.border = "";
-      var data = event.dataTransfer.getData("Text");
-      event.target.appendChild(document.getElementById(data));
-    }
-  });
-
 
 //load
 
 window.addEventListener('load', () => {
-    alert(`This page has loaded sucessfully`)
+    // alert(`This page has loaded sucessfully`)
 })
 
-// Using your index.js file, create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
-//focus
+//double Click
+
+const para = document.querySelectorAll('p');
+
+para.forEach((p) => {
+    p.addEventListener('dblclick', () => {
+        p.style.backgroundColor = "yellow";
+    })
+})
+
 //resize
+
+window.addEventListener('resize', () => {
+ 
+    console.log(`The window is ${window.innerHeight} high, and ${window.innerWidth} wide.`)
+})
+
+
 //scroll
+
+// Using your index.js file, create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
 //select
-//dblclick
+
 //Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
 //Nest two similar events somewhere in the site and prevent the event propagation properly
